e2e442e0cbfd42b09931962ea530012a7b29c5e5
David D'Amico <human@ddamico.org>
2019-01-04 14:23:34 -0500

    guards for no mongo db connection string in .env, captures it into a local if present

--- nightscout-graphql-server.js
+++ nightscout-graphql-server.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 
+if (!process.env.MDB_CONNECTION_STRING
+	|| process.env.MDB_CONNECTION_STRING && process.env.MDB_CONNECTION_STRING === '') {
+	console.log('No data store connection string specified, make sure MDB_CONNECTION_STRING is set in your .env');
+	process.exit(1);
+}
+
+const mdbc = process.env.MDB_CONNECTION_STRING;
