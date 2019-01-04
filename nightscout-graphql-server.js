2a8f8d743f6a1b195c566fef7619a692f8089523
David D'Amico <human@ddamico.org>
2019-01-04 14:29:34 -0500

    adds basic setup of mdb client connection

--- nightscout-graphql-server.js
+++ nightscout-graphql-server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
+
 
 if (!process.env.MDB_CONNECTION_STRING
 	|| process.env.MDB_CONNECTION_STRING && process.env.MDB_CONNECTION_STRING === '') {
@@ -7,9 +9,18 @@ if (!process.env.MDB_CONNECTION_STRING
 	process.exit(1);
 }
 
+const app = express();
 const mdbc = process.env.MDB_CONNECTION_STRING;
-
+MongoClient.connect(mdbc, (error, client) => {
+	if (error) {
+		console.log(error);
+		console.log('Or to paraphrase, something went wrong connecting to mongodb.');
+		process.exit(1);
+	}
+	app.listen(4000, () => {
+		console.log('Express Server Now Running On localhost:4000');
+	});
+});
 
 // express server and gql endpoint
-const app = express();
-app.listen(4000, () => console.log('Express Server Now Running On localhost:4000'));
+
